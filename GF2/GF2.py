42b1ec6c01f166f81c303f1cb85f074a5219e306
BASELINE <support@yrhenogledd.space>
2019-02-25 14:34:56 +0000

    GF2/1

--- GF2/GF2.py
+++ GF2/GF2.py
@@ -6,77 +6,62 @@ from .utils.dataIO import dataIO
 import os
 
 
-class GRA:
+class GF2:
 
     def __init__(self, bot):
         self.bot = bot
-        self.settings = dataIO.load_json("data/GRA/settings.json")
-    
+        self.settings = dataIO.load_json("data/GF2/settings.json")
+
     def savefile(self):
-        dataIO.save_json("data/GRA/settings.json", self.settings)
-    
+        dataIO.save_json("data/GF2/settings.json", self.settings)
+
     @commands.group(pass_context=True)
-    async def GRA(self, ctx):
+    async def GF2(self, ctx):
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
 
-    @GRA.command(pass_context=True, name="set", aliases=["setup"])
+    @GF2.command(pass_context=True, name="set", aliases=["setup"])
     async def _set(self, ctx, channel:discord.Channel, role:discord.Role=None):
-        """Setup the rules channel and role to be applied"""
+        """Setup the GF2 channel and role to be applied"""
         if channel is None:
             channel = ctx.message.channel
         role_id = None
         if role is not None:
             role_id = role.id
-        self.settings[ctx.message.server.id] = {"rules": "Welcome! Please react with 🇾 to accept the rules.", "channel": channel.id,
+        self.settings[ctx.message.server.id] = {"GF2": "Welcome! Please react with 🇾 to accept the GF2.", "channel": channel.id,
         "role": role_id}
         self.savefile()
-        await self.bot.say("Rules set to {} in channel {} applying role {}".format(self.settings[ctx.message.server.id]["rules"], channel.mention, role))
-        
-        self.settings[ctx.message.server.id] = {"rules": "Welcome! Please react with 🇾 to accept the rules.", "channel": channel.id,
-        "unrole": role_id}
-        self.savefile()
-        await self.bot.say("Rules set to {} in channel {} applying role {}".format(self.settings[ctx.message.server.id]["rules"], channel.mention, role))
-    
-    @GRA.command(pass_context=True)
+        await self.bot.say("GF2 set to {} in channel {} applying role {}".format(self.settings[ctx.message.server.id]["GF2"], channel.mention, role))
+
+    @GF2.command(pass_context=True)
     async def channel(self, ctx, channel: discord.Channel):
-        """Set the rules channel to be accepted in"""
+        """Set the GF2 channel to be accepted in"""
         server = ctx.message.server
         if server.id not in self.settings:
-            await self.bot.say("Please use the rules set command to change the rules channel")
+            await self.bot.say("Please use the GF2 set command to change the GF2 channel")
             return
         self.settings[ctx.message.server.id]["channel"] = channel.id
         await self.bot.say("Channel changed to {}".format(channel.mention))
         self.savefile()
-    
-    @GRA.command(pass_context=True)
+
+    @GF2.command(pass_context=True)
     async def change(self, ctx, *, message):
-        """Set the rules message to be sent"""
+        """Set the GF2 message to be sent"""
         if ctx.message.server.id not in self.settings:
-            await self.bot.say("Please use the rules set command to change the rules message")
+            await self.bot.say("Please use the GF2 set command to change the GF2 message")
             return
-        self.settings[ctx.message.server.id]["rules"] = message
+        self.settings[ctx.message.server.id]["GF2"] = message
         await self.bot.say("Message changed to {}".format(message))
         self.savefile()
-        
-    @GRA.command(pass_context=True)
+
+    @GF2.command(pass_context=True)
     async def role(self, ctx, role:discord.Role):
-        """Set the role to be applied when rules are accepted"""
+        """Set the role to be applied when GF2 are accepted"""
         if ctx.message.server.id not in self.settings:
-            await self.bot.say("Please use the rules set command to change the role added")
+            await self.bot.say("Please use the GF2 set command to change the role added")
             return
         self.settings[ctx.message.server.id]["role"] = role.id
         await self.bot.say("Role changed to {}".format(role.name))
-        self.savefile()
-
-    @GRA.command(pass_context=True)
-    async def unrole(self, ctx, role:discord.Role):
-        """Set the role to be removed when rules are accepted"""
-        if ctx.message.server.id not in self.settings:
-            await self.bot.say("Please use the rules set command to change the role removed")
-            return
-        self.settings[ctx.message.server.id]["unrole"] = role.id
-        await self.bot.say("Role removed {}".format(role.name))
         self.savefile()
 
 
@@ -85,7 +70,7 @@ class GRA:
         if server.id not in self.settings:
             return
         channel = discord.Object(id=self.settings[server.id]["channel"])
-        message = await self.bot.send_message(channel, "{} {}".format(member.mention, self.settings[server.id]["rules"]))
+        message = await self.bot.send_message(channel, "{} {}".format(member.mention, self.settings[server.id]["GF2"]))
         role_id = self.settings[server.id]["role"]
         if role_id is None:
             return
@@ -100,21 +85,20 @@ class GRA:
         answer = await self.bot.wait_for_reaction(emoji=["🇾", "🇳"], user=member, message=message)
         if answer.reaction.emoji == "🇾":
             await self.bot.add_roles(member, role)
-            await self.bot.remove_roles(member, unrole)
             await self.bot.delete_message(message)
         if answer.reaction.emoji == "🇳":
             await self.bot.kick(member)
             await self.bot.delete_message(message)
-            
+
 def check_folder():
-    if not os.path.exists("data/GRA"):
-        print("Creating data/acceptrules folder")
-        os.makedirs("data/GRA")
+    if not os.path.exists("data/GF2"):
+        print("Creating data/GF2 folder")
+        os.makedirs("data/GF2")
 
 
 def check_file():
     data = {}
-    f = "data/GRA/settings.json"
+    f = "data/GF2/settings.json"
     if not dataIO.is_valid_json(f):
         print("Creating default settings.json...")
         dataIO.save_json(f, data)
@@ -123,6 +107,5 @@ def check_file():
 def setup(bot):
     check_folder()
     check_file()
-    n = GRA(bot)
+    n = GF2(bot)
     bot.add_cog(n)
-
